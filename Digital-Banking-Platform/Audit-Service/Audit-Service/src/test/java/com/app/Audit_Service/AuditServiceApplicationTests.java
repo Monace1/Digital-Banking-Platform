491895853853fbf672dc060d5de2bedcf8a20bc4
Monanii <morganchuma654@gmail.com>
2025-03-10 22:32:54 -0700

    Transaction-service,Notification-service

--- Digital-Banking-Platform/Audit-Service/Audit-Service/src/test/java/com/app/Audit_Service/AuditServiceApplicationTests.java
+++ Digital-Banking-Platform/Audit-Service/Audit-Service/src/test/java/com/app/Audit_Service/AuditServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.Audit_Service;
+package com.app.Audit_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ Digital-Banking-Platform/Transaction-Service/Transaction-Service/src/main/java/com/app/Transaction_Service/transactions/Transaction.java
@@ -0,0 +1,35 @@
+package com.app.Transaction_Service.transactions;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
+
